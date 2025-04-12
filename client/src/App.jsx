7ea8079dd3cb783e1b917f29d1a9edb153863be2
Ramanchi Rishi Sai Teja <102644611+xenon1919@users.noreply.github.com>
2025-04-12 23:50:38 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,26 +8,14 @@ function App() {
   const bottomRef = useRef(null);
 
   const fixedCategories = [
-    "smartphones",
     "laptops",
-    "fragrances",
-    "skincare",
-    "groceries",
-    "home-decoration",
-    "furniture",
     "tops",
     "womens-dresses",
     "womens-shoes",
-    "mens-shirts",
     "mens-shoes",
     "mens-watches",
     "womens-watches",
     "womens-bags",
-    "womens-jewellery",
-    "sunglasses",
-    "automotive",
-    "motorcycle",
-    "lighting",
   ];
 
   useEffect(() => {
